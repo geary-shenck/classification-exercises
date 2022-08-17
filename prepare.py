5b595afb11902973ad2fc64f4cbcd5cfceea850b
Geary Shenck <geary.shenck@gmail.com>
2022-08-17 11:12:24 -0500

    added function for titanic to do LR for age)

--- prepare.py
+++ prepare.py
@@ -1,5 +1,7 @@
 
 from sklearn.model_selection import train_test_split
+
+from sklearn.linear_model import LinearRegression
 import pandas as pd
 import numpy as np
 import acquire
@@ -30,9 +32,10 @@ def prep_iris():
     iris_df.rename(columns = {"species_name":"species"},inplace=True)
     iris_df.drop(columns=["species_id","measurement_id"],inplace=True)
     #dummpy_df = pd.get_dummies(iris_df[["species"]],drop_first=False)
-    dummpy_df = pd.get_dummies(iris_df,drop_first=False)
-    df = pd.concat([iris_df,dummpy_df],axis=1)
-    return df
+    #dummpy_df = pd.get_dummies(iris_df,drop_first=False)
+    #df = pd.concat([iris_df,dummpy_df],axis=1)
+    #return dummpy_df
+    return iris_df
 
 def prep_titanic():
     ''' 
@@ -42,14 +45,21 @@ def prep_titanic():
     makes a dummy_df out of sex, class_paid, deck, embark_town
     returns a df that's concatenated out of modified and dummy
     ''' 
-    titanic_df = acquire.get_titanic_data()
-    titanic_df.drop(columns=["embarked","pclass"],inplace=True)
-    titanic_df.drop(columns=["age","deck"],inplace=True) ##due to nulls
-    titanic_df.rename(columns = {"class":"class_paid"},inplace=True)
-    #dummy_df = pd.get_dummies(titanic_df[["sex","class_paid","deck","embark_town"]],drop_first=False)
-    dummy_df = pd.get_dummies(titanic_df,drop_first=False)
-    df = pd.concat([titanic_df,dummy_df],axis=1)
-    return df
+    titanic = acquire.get_titanic_data()
+    #titanic_df.drop(columns=["embarked","pclass"],inplace=True)
+    #titanic_df.drop(columns=["age","deck"],inplace=True) ##due to nulls
+    #titanic_df.rename(columns = {"class":"class_paid"},inplace=True)
+    ##dummy_df = pd.get_dummies(titanic_df[["sex","class_paid","deck","embark_town"]],drop_first=False)
+    #dummy_df = pd.get_dummies(titanic_df,drop_first=False)
+    #df = pd.concat([titanic_df,dummy_df],axis=1)
+
+    ## below matches with class to follow along
+
+    titanic = titanic.drop(columns=['embarked','class', 'age','deck'])
+    dummy_df = pd.get_dummies(data=titanic[['sex','embark_town']], drop_first=True)
+    titanic = pd.concat([titanic, dummy_df], axis=1)
+    
+    return titanic
 
 def prep_telco():
     ''' 
@@ -84,6 +94,70 @@ def prep_telco():
                             ]],
                             drop_first=True)
     df = pd.concat([telco_churn_df,dummy_df],axis=1)
+    df.drop(columns = [
+                            'multiple_lines',
+                            "dependents",
+                            "phone_service",
+                            "customer_id",
+                            "paperless_billing",
+                            "gender",
+                            "partner",
+                            "churn",
+                            'online_security',
+                            'online_backup',
+                            'device_protection',
+                            'tech_support',
+                            'streaming_tv',
+                            'streaming_movies',
+                            'contract_type',
+                            'internet_service_type',
+                            'payment_type'
+                            ],inplace=True)
     df.head()
 ## will update to match class
+    return df
+
+def prep_titanic_further():
+    ''' 
+    takes in titanic data using get_titanic_data()
+    drops duplicated column info
+    renames class to class paid
+    makes a dummy_df out of sex, class_paid, deck, embark_town
+    returns a df that's concatenated out of modified and dummy
+    ''' 
+    df = acquire.get_titanic_data()
+
+    df.drop(columns=["deck","embarked","class"],inplace=True)
+    df.sex = df.sex.map({"male":0,"female":1})
+    df_w_null = df[df.isnull().any(axis=1)==1][["survived","pclass","sex","sibsp","parch","fare","alone","age"]]
+
+    df_wo_null = df[df.isnull().any(axis=1)==0][["survived","pclass","sex","sibsp","parch","fare","alone","age"]]
+
+    temp_df = pd.DataFrame(columns=["age"])
+
+    linreg=LinearRegression()
+
+    X_titanic_null_train = df_wo_null.iloc[:,:7]
+    y_titanic_null_train = df_wo_null.iloc[:,7]
+
+    linreg.fit(X=X_titanic_null_train, y=y_titanic_null_train)
+
+    test_data = df_w_null.iloc[:,:7]
+
+    temp_df["age"] = pd.DataFrame(linreg.predict(test_data))
+
+    df_w_null.drop(columns=["age"],inplace=True)
+
+    temp_df.index = df_w_null.index
+
+    df_w_null = pd.concat([df_w_null, temp_df],axis=1,ignore_index=False)
+    df_w_null["age"].clip(lower=.5)
+
+    df.age.fillna(df_w_null["age"],inplace=True)
+
+    df.embark_town.fillna(df.embark_town.mode()[0],inplace=True)
+
+    dummy_df = pd.get_dummies(data=df[['sex','embark_town']], drop_first=True)
+    df = pd.concat([df, dummy_df], axis=1)
+
     return df
